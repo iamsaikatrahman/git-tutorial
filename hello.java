5cd5095adec2cae34bec31968ee9f3bb6cd96518
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-06-08 20:13:40 +0600

    keep both method

--- hello.java
+++ hello.java
@@ -3,8 +3,14 @@ public class Hello{
         System.out.println("Vs Code is Awesome...");
         display();
     }
+     public static void greeting(String name){
+        System.out.println("Welcome " + name);
+    }
     public static void display(){
         System.out.println("Hello Hello");
+    }
+    public static int sqr(int x){
+        return x*x;
     }
 
 }
