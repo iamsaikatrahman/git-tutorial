407e944ae6e1838e37b71120e5583fe3b13baab8
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-06-08 19:02:21 +0600

    change greating method

--- hello.java
+++ hello.java
@@ -1,10 +1,10 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Vs Code is Awesome...");
-        greeting("Saikat Rahman");
+        greeting("Git");
     }
 public static void greeting(String name){
-    System.out.println("Welcome " + name);
+    System.out.println("Bye Bye " + name);
 }
 
 }
