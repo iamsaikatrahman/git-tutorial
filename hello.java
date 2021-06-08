a8b4b9aea0a4b646de712f9df3e4cbc4c5a94e24
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-06-08 18:37:51 +0600

    Added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Vs Code is Awesome...");
+        greeting("Saikat Rahman");
     }
+public static void greeting(String name){
+    System.out.println("Welcome " + name);
+}
+
 }
